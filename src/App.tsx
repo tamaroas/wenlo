7a83802c090c06bbcfe502f23f6ed391d12fe859
BrightkyEfoo <brightefoo@gmail.com>
2023-10-14 20:12:52 +0100

    ajout du systeme de navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
+import React from 'react';
 import './App.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import LoginRegister from './pages/Login-Register/LoginRegister';
+const router = createBrowserRouter([
+  {
+    path: "/register",
+    element: <LoginRegister />,
+  },
+  {
+    path: "/login",
+    element: <LoginRegister />,
+  },
+]);
+
 
 function App() {
   return (
-    <h1>Hello Wenlo</h1>
+    <RouterProvider router={router} />
   );
 }
 
