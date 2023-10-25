4ae1a8e00e8f1b8bbee8d569a33592c304cbfe17
BrightkyEfoo <brightefoo@gmail.com>
2023-10-25 04:09:59 +0100

    login -register -password

---
+++ src/pages/Pricing/Index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './Pricing.module.css';
+const Index = () => {
+  return (
+    <div className={styles.container}>
+      <img className={styles.image1} src="images/Group11.svg" />
+
+
+        <img />
+
+      <img className={styles.image2} src="images/Group12.svg" />
+    </div>
+  );
+};
+
+export default Index;
