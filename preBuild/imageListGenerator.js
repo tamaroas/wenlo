cbe88694c8062f6f56a6abdf40d5f5230526e14c
BrightkyEfoo <brightefoo@gmail.com>
2023-11-19 23:52:39 +0100

    change on prebuild

--- preBuild/imageListGenerator.js
+++ preBuild/imageListGenerator.js
@@ -2,4 +2,7 @@ const fs = require('fs');
 
 const files = fs.readdirSync('./public/images');
 console.log('files', files);
-fs.writeFileSync('./src/utils/imagesList.json', JSON.stringify(files));
+let res = {};
+files.forEach(el => (res[el] = el));
+
+fs.writeFileSync('./src/utils/imagesList.json', JSON.stringify(res));
