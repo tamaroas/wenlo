cc6fc7118ebbbbebcb51bffe7990b8b0b1ce413f
BrightkyEfoo <brightefoo@gmail.com>
2023-11-30 20:47:33 +0100

    Content 2 of dashboard Main balance

--- src/components/Dashboard/Content/Content2-Balance.tsx
+++ src/components/Dashboard/Content/Content2-Balance.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import { Section7, Section9 } from './Content1-Main';
+import styles from './Content2-Main.module.scss';
 
 const Balance = () => {
-  return <div>Balance Balance</div>;
+  return (
+    <div className={styles.container}>
+      <h2>Main Balance Management</h2>
+      <Section7 />
+      <Section9 />
+    </div>
+  );
 };
 
 export default Balance;
