e36777642eb5a1820a8bd9b4984018936e5839e0
BrightkyEfoo <brightefoo@gmail.com>
2023-12-17 00:47:45 +0100

    add alert or popup on success of deposit with style

--- src/components/Dashboard/Content/MainBalanceDeposit.tsx
+++ src/components/Dashboard/Content/MainBalanceDeposit.tsx
@@ -5,11 +5,14 @@ import { FiCopy } from 'react-icons/fi';
 import { CiImageOn } from 'react-icons/ci';
 import { AiOutlineExclamationCircle } from 'react-icons/ai';
 import imageService from '../../../utils/ImageService';
+import { FaCircleCheck } from 'react-icons/fa6';
 
 const images = imageService.getImages();
 const emitter = new Emitter();
 const eventsNames = {
   SET_BANK: 'SET_BANK',
+  CLOSE_ALERT: 'CLOSE_ALERT',
+  SET_ALERT_DATA: 'SET_ALERT_DATA',
 };
 
 type FormData = {
@@ -43,6 +46,7 @@ const banks = [
   },
 ];
 const MainBalanceDeposit = () => {
+  const [isAlertVisible, setIsAlertVisible] = useState(true);
   const [formData, setFormData] = useState<FormData>({
     bank: '',
     address: '',
@@ -51,6 +55,15 @@ const MainBalanceDeposit = () => {
     image: undefined,
   });
 
+  const handleSubmit = () => {
+    emitter.emit(eventsNames.SET_ALERT_DATA, {
+      state: 'success',
+      title: 'Succesfully!',
+      desc: 'Your deposit request was sent successfully',
+    });
+    setIsAlertVisible(true);
+  };
+
   emitter.on(eventsNames.SET_BANK, (value: string) => {
     setFormData(prev => {
       let temp = { ...prev };
@@ -58,73 +71,117 @@ const MainBalanceDeposit = () => {
       return temp;
     });
   });
+
+  emitter.on(eventsNames.CLOSE_ALERT, () => {
+    setIsAlertVisible(false);
+  });
   return (
-    <div className={styles.container}>
-      <div className={styles.subContainer}>
-        <div>
-          <div className={styles.logoContainer}>
-            <img
-              src={images['credit-card-plus']}
-              alt="credit-card-plus"
-              height={40}
-              width={40}
-            />
+    <>
+      <div className={styles.container}>
+        <div className={styles.subContainer}>
+          <div>
+            <div className={styles.logoContainer}>
+              <img
+                src={images['credit-card-plus']}
+                alt="credit-card-plus"
+                height={40}
+                width={40}
+              />
+            </div>
+            <h2>Deposit Balance</h2>
           </div>
-          <h2>Deposit Balance</h2>
-        </div>
-        <span>
-          Choosed payment method<span style={{ color: 'red' }}>*</span>
-        </span>
-        <div className={styles.section1}>
           <span>
-            After choosed the payment method of your choice, make the payment
-            according to the details displayed below.
+            Choosed payment method<span style={{ color: 'red' }}>*</span>
           </span>
-          <div className={styles.banksContainer}>
-            {banks.map((el, idx) => {
-              return (
-                <BlockRadio
-                  key={'blockRadio-Bank' + idx}
-                  {...el}
-                  selected={formData.bank === el.value}
-                />
-              );
-            })}
+          <div className={styles.section1}>
+            <span>
+              After choosed the payment method of your choice, make the payment
+              according to the details displayed below.
+            </span>
+            <div className={styles.banksContainer}>
+              {banks.map((el, idx) => {
+                return (
+                  <BlockRadio
+                    key={'blockRadio-Bank' + idx}
+                    {...el}
+                    selected={formData.bank === el.value}
+                  />
+                );
+              })}
+            </div>
+            <span>USDT TRC20 Address</span>
+            <div className={styles.bankAddressInputContainer}>
+              <input type="text" />
+              <FiCopy />
+            </div>
           </div>
-          <span>USDT TRC20 Address</span>
-          <div className={styles.bankAddressInputContainer}>
-            <input type="text" />
-            <FiCopy />
-          </div>
-        </div>
-        <span>
-          Amount of the deposit in $<span style={{ color: 'red' }}>*</span>
-        </span>
-        <input type="number" placeholder="Enter your Amount" />
-        <div>
           <span>
-            Transaction reference<span style={{ color: 'red' }}>*</span>
+            Amount of the deposit in $<span style={{ color: 'red' }}>*</span>
           </span>
-          <span>(Provide the transaction ID your payment)</span>
+          <input type="number" placeholder="Enter your Amount" />
+          <div>
+            <span>
+              Transaction reference<span style={{ color: 'red' }}>*</span>
+            </span>
+            <span>(Provide the transaction ID your payment)</span>
+          </div>
+          <input type="text" placeholder="Enter your transaction reference" />
+          <span>Payment proof (Image or Pdf file)</span>
+          <div className={styles.proofContainer}>
+            <CiImageOn size={24} />
+            <p>
+              <span style={{ color: '#3949A1', cursor: 'pointer' }}>
+                Click to Upload
+              </span>{' '}
+              or drag and drop <br />
+              DOC, PDF, PNG or JPG (max 10mb)
+            </p>
+          </div>
+          <span className={styles.warranty}>
+            <AiOutlineExclamationCircle />
+            Take a few secondes to verify filled informations, in order to avoid
+            erros and request rejection !!
+          </span>
+          <button onClick={handleSubmit}>Submit</button>
         </div>
-        <input type="text" placeholder="Enter your transaction reference" />
-        <span>Payment proof (Image or Pdf file)</span>
-        <div className={styles.proofContainer}>
-          <CiImageOn size={24} />
-          <p>
-            <span style={{ color: '#3949A1', cursor: 'pointer' }}>
-              Click to Upload
-            </span>{' '}
-            or drag and drop <br />
-            DOC, PDF, PNG or JPG (max 10mb)
-          </p>
-        </div>
-        <span className={styles.warranty}>
-          <AiOutlineExclamationCircle />
-          Take a few secondes to verify filled informations, in order to avoid
-          erros and request rejection !!
-        </span>
-        <button>Submit</button>
+      </div>
+      {isAlertVisible && <Alert />}
+    </>
+  );
+};
+
+type AlertState = {
+  state: 'success';
+  title: 'Succesfully!';
+  desc: 'Your deposit request was sent successfully';
+};
+
+const Alert = () => {
+  const [data, setData] = useState<AlertState>({
+    state: 'success',
+    title: 'Succesfully!',
+    desc: 'Your deposit request was sent successfully',
+  });
+
+  emitter.on(eventsNames.SET_ALERT_DATA, (eventData: AlertState) => {
+    setData(prev => eventData);
+  });
+
+  const handleGoBack = () => {
+    emitter.emit(eventsNames.CLOSE_ALERT, undefined);
+  };
+  return (
+    <div
+      className={styles.alertContainer}
+      onClick={e => {
+        if (e.target === e.currentTarget) handleGoBack();
+      }}
+    >
+      <div className={styles.alertSubContainer}>
+        <FaCircleCheck size={80} color="#1CB465" />
+        <span>{data.title}</span>
+        <span>{data.desc}</span>
+        <button onClick={handleGoBack}>Go Back</button>
       </div>
     </div>
   );
