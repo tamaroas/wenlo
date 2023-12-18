bbcb6202f4b06eb893d4f862186bd45c10d7edae
BrightkyEfoo <brightefoo@gmail.com>
2023-12-18 10:50:59 +0100

    Create and style withdraw componnent

---
+++ src/components/Dashboard/Content/MainBalanceWithdrawal.tsx
@@ -0,0 +1,283 @@
+import React, { useState } from 'react';
+import Emitter from '../../../utils/EventEmitter/EventEmitter';
+import styles from './MainBalanceWithdrawal.module.scss';
+import { FiCopy } from 'react-icons/fi';
+import { CiImageOn } from 'react-icons/ci';
+import { AiOutlineExclamationCircle } from 'react-icons/ai';
+import imageService from '../../../utils/ImageService';
+import { FaCircleCheck } from 'react-icons/fa6';
+
+const images = imageService.getImages();
+const emitter = new Emitter();
+const eventsNames = {
+  SET_BANK: 'SET_BANK',
+  CLOSE_ALERT: 'CLOSE_ALERT',
+  SET_ALERT_DATA: 'SET_ALERT_DATA',
+};
+
+type FormData = {
+  bank: string;
+  address: string;
+  amount: string;
+  reference: string;
+  image: any;
+};
+const RenderBankNeededDetails = (bankvalue: string) => {
+  switch (bankvalue) {
+    case 'USDTTRC20':
+      return <USDTTRC20Inputs />;
+    case 'BankWire':
+      return <BankWire />;
+    case 'Payoneer':
+      return <Payoneer />;
+    case 'Wise':
+      return <Wise />;
+    case 'CreditCard':
+      return <CreditCard />;
+    default:
+      return 'Yo';
+  }
+};
+
+const banks = [
+  {
+    text: 'USDT TRC20',
+    value: 'USDTTRC20',
+  },
+  {
+    text: 'Bank Wire (USD)',
+    value: 'BankWire',
+  },
+  {
+    text: 'Payoneer',
+    value: 'Payoneer',
+  },
+  {
+    text: 'Wise',
+    value: 'Wise',
+  },
+  {
+    text: 'Credit Card',
+    value: 'CreditCard',
+  },
+];
+
+const USDTTRC20Inputs = () => {
+  return (
+    <>
+      <span>USDT TRC20 Address</span>
+      <div className={styles.bankAddressInputContainer}>
+        <input type="text" />
+        <FiCopy />
+      </div>
+    </>
+  );
+};
+
+const BankWire = () => {
+  return (
+    <>
+      <label>Beneficiery Name</label>
+      <input type="text" placeholder="Enter your Beneficiery Name" />
+      <label>Account Number</label>
+      <input type="text" placeholder="Enter your Account Number" />
+      <label>SWIFT Code</label>
+      <input type="text" placeholder="Enter your SWIFT Code" />
+      <label>Account Address</label>
+      <input type="text" placeholder="Enter your Account Address" />
+    </>
+  );
+};
+
+const Payoneer = () => {
+  return (
+    <>
+      <label>Payoneer Email address</label>
+      <input type="email" placeholder="Enter your Email" />
+    </>
+  );
+};
+
+const Wise = () => {
+  return (
+    <>
+      <label>Email</label>
+      <input type="text" placeholder="Enter your Email" />
+      <label>Bank Name</label>
+      <input type="text" placeholder="Enter your Bank Name" />
+      <label>Bank Address</label>
+      <input type="text" placeholder="Enter your Bank Address" />
+      <label>Routing (ABA)</label>
+      <input type="text" placeholder="Enter your Routing (ABA)" />
+      <label>Account Number</label>
+      <input type="text" placeholder="Enter your Account Number" />
+      <label>Account Type</label>
+      <input type="text" placeholder="Enter your Account Type" />
+      <label>Beneficiary Name</label>
+      <input type="text" placeholder="Enter your Beneficiary Name" />
+    </>
+  );
+};
+
+const CreditCard = () => {
+  return (
+    <>
+      <span>USDT TRC20 Address</span>
+      <div className={styles.bankAddressInputContainer}>
+        <input type="text" />
+        <FiCopy />
+      </div>
+    </>
+  );
+};
+
+const MainBalanceWithdrawal = () => {
+  const [isAlertVisible, setIsAlertVisible] = useState(false);
+  const [formData, setFormData] = useState<FormData>({
+    bank: 'USDTTRC20',
+    address: '',
+    amount: '',
+    reference: '',
+    image: undefined,
+  });
+
+  const handleSubmit = () => {
+    emitter.emit(eventsNames.SET_ALERT_DATA, {
+      state: 'success',
+      title: 'Succesfully!',
+      desc: 'Your withdrawal request will be processed immediately, it will take 1-2 weeks',
+    });
+    setIsAlertVisible(true);
+  };
+
+  emitter.on(eventsNames.SET_BANK, (value: string) => {
+    setFormData(prev => {
+      let temp = { ...prev };
+      temp.bank = value;
+      return temp;
+    });
+  });
+
+  emitter.on(eventsNames.CLOSE_ALERT, () => {
+    setIsAlertVisible(false);
+  });
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.subContainer}>
+          <div>
+            <div className={styles.logoContainer}>
+              <img
+                src={images['credit-card-download']}
+                alt="credit-card-plus"
+                height={40}
+                width={40}
+              />
+            </div>
+            <h2>Withdrawal Balance</h2>
+          </div>
+
+          <span>
+            Amount of the withdrawal in $<span style={{ color: 'red' }}>*</span>
+          </span>
+          <input type="number" placeholder="Enter your Amount" />
+
+          <span>
+            Choosed payment method<span style={{ color: 'red' }}>*</span>
+          </span>
+          <div className={styles.section1}>
+            <span>
+              After choosed the payment method of your choice, make the payment
+              according to the details displayed below.
+            </span>
+            <div className={styles.banksContainer}>
+              {banks.map((el, idx) => {
+                return (
+                  <BlockRadio
+                    key={'blockRadio-Bank' + idx}
+                    {...el}
+                    selected={formData.bank === el.value}
+                  />
+                );
+              })}
+            </div>
+            {RenderBankNeededDetails(formData.bank)}
+          </div>
+
+          <button onClick={handleSubmit}>Submit</button>
+        </div>
+      </div>
+      {isAlertVisible && <Alert />}
+    </>
+  );
+};
+
+type AlertState = {
+  state: 'success';
+  title: 'Succesfully!';
+  desc: 'Your withdrawal request will be processed immediately, it will take 1-2 weeks';
+};
+
+const Alert = () => {
+  const [data, setData] = useState<AlertState>({
+    state: 'success',
+    title: 'Succesfully!',
+    desc: 'Your withdrawal request will be processed immediately, it will take 1-2 weeks',
+  });
+
+  emitter.on(eventsNames.SET_ALERT_DATA, (eventData: AlertState) => {
+    setData(prev => eventData);
+  });
+
+  const handleGoBack = () => {
+    emitter.emit(eventsNames.CLOSE_ALERT, undefined);
+  };
+  return (
+    <div
+      className={styles.alertContainer}
+      onClick={e => {
+        if (e.target === e.currentTarget) handleGoBack();
+      }}
+    >
+      <div className={styles.alertSubContainer}>
+        <FaCircleCheck size={80} color="#1CB465" />
+        <span>{data.title}</span>
+        <span>{data.desc}</span>
+        <button onClick={handleGoBack}>Go Back</button>
+      </div>
+    </div>
+  );
+};
+
+const BlockRadio = ({
+  text,
+  value,
+  selected,
+}: {
+  text: string;
+  value: string;
+  selected?: boolean;
+}) => {
+  const handleClick = () => {
+    emitter.emit(eventsNames.SET_BANK, value);
+  };
+  return (
+    <div
+      className={
+        selected
+          ? styles.blockRadioContainerSelected
+          : styles.blockRadioContainerNotSelected
+      }
+      onClick={handleClick}
+    >
+      <div
+        className={
+          selected ? styles.blockRadioSelected : styles.blockRadioNotSelected
+        }
+      ></div>
+      <span>{text}</span>
+    </div>
+  );
+};
+
+export default MainBalanceWithdrawal;
